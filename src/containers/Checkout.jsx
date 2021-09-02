5179daf1a96127e8b2ff307d88097757bf1f6b69
Joseph Salen <jsalen.91@gmail.com>
2021-09-01 22:11:03 -0400

    add react-helmet and meta tags

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,9 +1,19 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import MetaHead from '../components/MetaHead';
 
 import { AppContext } from '../context/AppContext';
 
 import '../styles/components/Checkout.css';
+
+const meta = (
+  <MetaHead
+    title="Checkout"
+    description="Encuentra todos tus productos favoritos"
+    image="https://davecast.s3.amazonaws.com/avatarnegativo.jpg"
+    url="https://fake-platzi-store-merch.web.app/"
+  />
+);
 
 const Checkout = () => {
   const {
@@ -23,30 +33,33 @@ const Checkout = () => {
   };
 
   return (
-    <div className="Checkout">
-      <div className="Checkout-content">
-        <h3>{cart.length > 0 ? 'Lista de Pedidos:' : 'No hay productos.'}</h3>
-        {cart.map((item, idx) => (
-          <div className="Checkout-item" key={idx}>
-            <div className="Checkout-element">
-              <h4>{item.title}</h4>
-              <span>${item.price}</span>
+    <>
+      {meta}
+      <div className="Checkout">
+        <div className="Checkout-content">
+          <h3>{cart.length > 0 ? 'Lista de Pedidos:' : 'No hay productos.'}</h3>
+          {cart.map((item, idx) => (
+            <div className="Checkout-item" key={idx}>
+              <div className="Checkout-element">
+                <h4>{item.title}</h4>
+                <span>${item.price}</span>
+              </div>
+              <button type="button" onClick={handleRemove(item)}>
+                <i className="fas fa-trash"></i>
+              </button>
             </div>
-            <button type="button" onClick={handleRemove(item)}>
-              <i className="fas fa-trash"></i>
-            </button>
-          </div>
-        ))}
-      </div>
-      {cart.length > 0 && (
-        <div className="Checkout-sidebar">
-          <h3>Precio Total: ${showSumTotal()}</h3>
-          <Link to="/checkout/information">
-            <button type="button">Continuar pedido</button>
-          </Link>
+          ))}
         </div>
-      )}
-    </div>
+        {cart.length > 0 && (
+          <div className="Checkout-sidebar">
+            <h3>Precio Total: ${showSumTotal()}</h3>
+            <Link to="/checkout/information">
+              <button type="button">Continuar pedido</button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import Products from '../components/Products';
+import MetaHead from '../components/MetaHead';
+import { Helmet } from 'react-helmet';
 
 import initialState from '../initialState';
 
+const meta = (
+  <MetaHead
+    title="Productos"
+    description="Encuentra todos tus productos favoritos"
+    image="https://davecast.s3.amazonaws.com/avatarnegativo.jpg"
+    url="https://fake-platzi-store-merch.web.app/"
+  />
+);
+
 const Home = () => {
-  return <Products products={initialState.products} />;
+  return (
+    <>
+      {meta}
+      <Products products={initialState.products} />
+    </>
+  );
 };
 
 export default Home;
